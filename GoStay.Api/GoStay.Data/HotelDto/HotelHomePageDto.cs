16d426b7ed0ad6ac25d9ca3e39165439907d2d2f
TamNM1992 <106048213+TamNM1992@users.noreply.github.com>
2022-11-22 15:33:48 +0700

    cmt

--- GoStay.Api/GoStay.Data/HotelDto/HotelHomePageDto.cs
+++ GoStay.Api/GoStay.Data/HotelDto/HotelHomePageDto.cs
@@ -2,16 +2,21 @@
 {
 	public class HotelHomePageDto
 	{
-		public int Id { get; set; }
-		public string HotelName { get; set; }
-		public string HotelAddress { get; set; }
-		public int? Rating { get; set; }
-		public double Review_score { get; set; }
-		public double? Lat_map { get; set; }
-		public double? Lon_map { get; set; }
-		public double? Discount { get; set; }
-		public decimal? OriginalPrice { get; set; }
-		public decimal? ActualPrice { get; set; }
-		public List<string?> Pictures { get; set; } = new List<string?>();
-	}
+        public int Id { get; set; }
+        public string HotelName { get; set; }
+        public string HotelAddress { get; set; }
+        public int? Rating { get; set; }
+        public decimal? AvgNight { get; set; }
+        public double Review_score { get; set; }
+        public double? Lat_map { get; set; }
+        public double? Lon_map { get; set; }
+        public double? Discount { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public decimal ActualPrice { get; set; }
+        public int? NumberReviewers { get; set; }
+        public int? PalletbedRoom { get; set; }
+
+        public List<string> Pictures { get; set; } = new List<string>();
+    }
+
 }
