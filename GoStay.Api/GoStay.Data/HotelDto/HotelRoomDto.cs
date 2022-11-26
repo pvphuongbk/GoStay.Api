bec82b8b30a7523305624d5be07656faeb72010a
TamNM1992 <106048213+TamNM1992@users.noreply.github.com>
2022-11-26 11:26:48 +0700

    tam2611

--- GoStay.Api/GoStay.Data/HotelDto/HotelRoomDto.cs
+++ GoStay.Api/GoStay.Data/HotelDto/HotelRoomDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GoStay.Data.ServiceDto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace GoStay.Data.HotelDto
 {
     public class HotelRoomDto
     {
+        public int? Id { get; set; }
         public int? Idhotel { get; set; }
         public string? Name { get; set; }
         public decimal? RoomSize { get; set; }
@@ -20,8 +22,11 @@ namespace GoStay.Data.HotelDto
         public byte? NumMature { get; set; }
         public byte? NumChild { get; set; }
         public byte? Palletbed { get; set; }
-        public int? ViewDirection { get; set; }
+        public string ViewDirection { get; set; }
         public DateTime? CheckInDate { get; set; }
         public DateTime? CheckOutDate { get; set; }
+        public List<string> Pictures { get; set; } = new List<string>();
+        public List<ServiceDetailHotelDto> Services { get; set; }
+
     }
 }
