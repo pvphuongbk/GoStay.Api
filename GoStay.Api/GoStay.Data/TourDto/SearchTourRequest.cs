4f5ee876c48cc3c7e4e6ccd4e26abebc02061656
TamNM1992 <106048213+TamNM1992@users.noreply.github.com>
2022-12-29 16:53:12 +0700

    2912v1

--- GoStay.Api/GoStay.Data/TourDto/SearchTourRequest.cs
+++ GoStay.Api/GoStay.Data/TourDto/SearchTourRequest.cs
@@ -14,6 +14,7 @@ namespace GoStay.Data.TourDto
         public int[]? Rating { get; set; }
         public DateTime? StartDate { get; set; }
         public DateTime? EndDate { get; set; }
+        public int? NumMature { get; set; }
         public int PageIndex { get; set; }
         public int PageSize { get; set; }
     }
