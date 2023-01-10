5f0c0a047850ebea9e2588b47c227e32d1e7be84
TamNM1992 <106048213+TamNM1992@users.noreply.github.com>
2023-01-10 08:51:21 +0700

    10012023v1

--- GoStay.Api/GoStay.Data/TourDto/TourContentDto.cs
+++ GoStay.Api/GoStay.Data/TourDto/TourContentDto.cs
@@ -35,9 +35,9 @@ namespace GoStay.Data.TourDto
         public List<TourDetailDto> TourDetails { get; set; }
 
         public int IdDistrictFrom { get; set; }
-        public string ProvinceFrom { get; set; }
+        public string DistrictFrom { get; set; }
         public List<int> IdDistrictTo { get; set; }
-        public List<string> ProvinceTo { get; set; }
+        public List<string> DistrictTo { get; set; }
 
         public List<string> Pictures { get; set; }
     }
