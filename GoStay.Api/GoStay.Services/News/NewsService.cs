7bfa3c2d2d6c6e51e1028c2ee02df9e292b71447
HungHX <Xuanhungbk25251325@gmail.com>
2024-08-05 14:44:02 +0700

    ssssss

--- GoStay.Api/GoStay.Services/News/NewsService.cs
+++ GoStay.Api/GoStay.Services/News/NewsService.cs
@@ -1147,7 +1147,7 @@ namespace GoStay.Services.Newss
             try
             {
                 List<NewsCategoryTotal> newsTopicTotals = new List<NewsCategoryTotal>();
-                var cate = _newsCategoryRepository.FindAll(x => x.Iddomain == IdDomain).Include(x => x.News.Where(y => y.Deleted!=1));
+                var cate = _newsCategoryRepository.FindAll(x => x.Iddomain == IdDomain).Include(x => x.News.Where(y => y.Deleted!=1 && y.Iddomain == IdDomain));
 
                 foreach (var item in cate)
                 {
